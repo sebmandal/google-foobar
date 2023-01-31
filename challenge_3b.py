73523f364155af6c255fedfb6f409e146289749c
Sebastian Mandal <sebastian.mandal@icloud.com>
2023-02-01 00:50:46 +0100

    officially under review by google recruiters <3

--- challenge_3b.py
+++ challenge_3b.py
@@ -1,33 +1,74 @@
 """
-You're almost ready to make your move to destroy the LAMBCHOP doomsday device, but the security checkpoints that guard the underlying systems of the LAMBCHOP are going to be a problem. You were able to take one down without tripping any alarms, which is great! Except that as Commander Lambda's assistant, you've learned that the checkpoints are about to come under automated review, which means that your sabotage will be discovered and your cover blown -- unless you can trick the automated review system.
+Bomb, Baby!
+===========
 
-To trick the system, you'll need to write a program to return the same security checksum that the bunny trainers would have after they would have checked all the workers through. Fortunately, Commander Lambda's desire for efficiency won't allow for hours-long lines, so the trainers at the checkpoint have found ways to quicken the pass-through rate. Instead of checking each and every worker coming through, the bunny trainers instead go over everyone in line while noting their worker IDs, then allow the line to fill back up. Once they've done that they go over the line again, this time leaving off the last worker. They continue doing this, leaving off one more worker from the line each time but recording the worker IDs of those they do check, until they skip the entire line, at which point they XOR the IDs of all the workers they noted into a checksum and then take off for lunch. Fortunately, the workers' orderly nature causes them to always line up in numerical order without any gaps.
+You're so close to destroying the LAMBCHOP doomsday device you can taste it! But in order to do so, you need to deploy special self-replicating bombs designed for you by the brightest scientists on Bunny Planet. There are two types: Mach bombs (M) and Facula bombs (F). The bombs, once released into the LAMBCHOP's inner workings, will automatically deploy to all the strategic points you've identified and destroy them at the same time. 
 
-For example, if the first worker in line has ID 0 and the security checkpoint line holds three workers, the process would look like this:
-0 1 2 /
-3 4 / 5
-6 / 7 8
-where the trainers' XOR (^) checksum is 0^1^2^3^4^6 == 2.
+But there's a few catches. First, the bombs self-replicate via one of two distinct processes: 
+Every Mach bomb retrieves a sync unit from a Facula bomb; for every Mach bomb, a Facula bomb is created;
+Every Facula bomb spontaneously creates a Mach bomb.
 
-Likewise, if the first worker has ID 17 and the checkpoint holds four workers, the process would look like:
-17 18 19 20 /
-21 22 23 / 24
-25 26 / 27 28
-29 / 30 31 32
-which produces the checksum 17^18^19^20^21^22^23^25^26^29 == 14.
+For example, if you had 3 Mach bombs and 2 Facula bombs, they could either produce 3 Mach bombs and 5 Facula bombs, or 5 Mach bombs and 2 Facula bombs. The replication process can be changed each cycle. 
 
-All worker IDs (including the first worker) are between 0 and 2000000000 inclusive, and the checkpoint line will always be at least 1 worker long.
+Second, you need to ensure that you have exactly the right number of Mach and Facula bombs to destroy the LAMBCHOP device. Too few, and the device might survive. Too many, and you might overload the mass capacitors and create a singularity at the heart of the space station - not good! 
 
-With this information, write a function solution(start, length) that will cover for the missing security checkpoint by outputting the same checksum the trainers would normally submit before lunch. You have just enough time to find out the ID of the first worker to be checked (start) and the length of the line (length) before the automatic review occurs, so your program must generate the proper checksum with just those two values.
+And finally, you were only able to smuggle one of each type of bomb - one Mach, one Facula - aboard the ship when you arrived, so that's all you have to start with. (Thus it may be impossible to deploy the bombs to destroy the LAMBCHOP, but that's not going to stop you from trying!) 
+
+You need to know how many replication cycles (generations) it will take to generate the correct amount of bombs to destroy the LAMBCHOP. Write a function solution(M, F) where M and F are the number of Mach and Facula bombs needed. Return the fewest number of generations (as a string) that need to pass before you'll have the exact number of bombs necessary to destroy the LAMBCHOP, or the string "impossible" if this can't be done! M and F will be string representations of positive integers no larger than 10^50. For example, if M = "2" and F = "1", one generation would need to pass, so the solution would be "1". However, if M = "2" and F = "4", it would not be possible.
+
+Languages
+=========
+
+To provide a Java solution, edit Solution.java
+To provide a Python solution, edit solution.py
+
+Test cases
+==========
+Your code should pass the following test cases.
+Note that it may also be run against hidden test cases not shown here.
+
+-- Java cases --
+Input:
+Solution.solution('2', '1')
+Output:
+    1
+
+Input:
+Solution.solution('4', '7')
+Output:
+    4
+
+-- Python cases --
+Input:
+solution.solution('4', '7')
+Output:
+    4
+
+Input:
+solution.solution('2', '1')
+Output:
+    1
 """
 
-def solution(start, length):
-    checksum = 0
-    for i in range(0, length):
-        line_start = start + i * length
-        line_end = line_start + length - i
-        line_checksum = 0
-        for j in range(line_start, line_end):
-            line_checksum ^= j
-        checksum ^= line_checksum
-    return checksum
+import fractions
+
+def solution(M, F):
+    m = int(M)
+    f = int(F)
+    if m == 1 and f == 1:
+        return "0"
+    elif fractions.gcd(m, f) != 1:
+        return "impossible"
+    else:
+        count = 0
+        while m != 1 and f != 1:
+            if m > f:
+                count += (m // f)
+                m = m % f
+            else:
+                count += (f // m)
+                f = f % m
+        return str(count + (m + f - 2))
+
+
+print(solution("2", "1"))
